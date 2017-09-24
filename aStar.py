1ed61ce0ba47af483f4af68b158b7fee9f1af2a4
MrCaiting <cwu72@illinois.edu>
2017-09-23 22:15:39 -0500

    a star wait for testing

--- aStar.py
+++ aStar.py
@@ -10,15 +10,14 @@ def a_star_search(maze, start, goal):
                  (1, -1), (-1, 1), (-1, -1)]    # quick neighbors fiding offset
     close_list = set()
     parents = {}        # List to hold all parents
-    path_cost = {}      # path cost that we have so far
+    node_expanded = 0
+    cost_sofar = 0
     g_score = {start: 0}
     f_score = {start: utility.heuristicFcn(start, goal)}
 
     frontier = utility.PriorityQueue()
     frontier.push(start, 0)
-
     parents[start] = None
-    path_cost[start] = None
 
     while not frontier.isEmpty():
         current = frontier.pop()
@@ -33,6 +32,7 @@ def a_star_search(maze, start, goal):
             while current in parents:
                 path.append(current)
                 current = parents[current]
+                cost_sofar += 1
             return path
 
         "If the current node is not our goal, we add it to the close list"
@@ -42,8 +42,29 @@ def a_star_search(maze, start, goal):
         for dx, dy in neighbors:
             neighbor = current[0] + dx, current[1] + dy
             g_temp = g_score[current] + utility.heuristicFcn(current, neighbor)
+            f_temp = g_temp + utility.heuristicFcn(neighbor, goal)
 
             "Need to make sure that the neighbor location is valid"
             if 0 <= neighbor[1] < maze.shape[1]:   # coordinate validation
                 if 0 <= neighbor[0] < maze.shape[0]:
-                    if maze[neighbor[0], neighbor[1]] ==
+                    "Check if the intended neighbor is a wall"
+                    if maze[neighbor[0], neighbor[1]] == "%":
+                        continue
+                else:          # if the y-coordinate is not valid
+                    continue
+            else:              # if the x-coordinate is not valid
+                continue
+
+            if neighbor in close_list:      # skip if the node is closed
+                continue
+            if g_temp >= g_score.get(neighbor, 0):
+                continue
+            if g_temp < g_score.get(neighbor, 0) or (neighbor not in [i[1] for
+                                                     i in frontier.heap]):
+                parents[neighbor] = current
+                g_score[neighbor] = g_temp
+                f_score[neighbor] = f_temp
+                frontier.push(neighbor, f_score[neighbor])
+                node_expanded += 1
+
+    return False
