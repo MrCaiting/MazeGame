d8fb1c82dbedab706fc4a0de4574967bd739ac59
andrewhere <zekunhu2@illinois.edu>
2017-09-24 22:37:54 -0500

    Add files via upload

--- DFS_BFS.py
+++ DFS_BFS.py
@@ -1,18 +1,12 @@
-import readTXT
 from collections import deque
 import time
-row, col = readTXT.get_size_txt('mediumMaze.txt')
-maze = readTXT.read_txt('mediumMaze.txt',row, col)
-start, end = readTXT.find_enter_exit(maze)
 
-height = len(maze)
-width = len(maze[0])
-
-found = "found"
 
 def pacman_bfs(maze,start,end):
     queue = deque([start])
     visited = set()
+    height = len(maze)
+    width = len(maze[0])
     clk = time.clock()
     while queue:
         cur = queue.popleft()
@@ -35,17 +29,18 @@ def pacman_bfs(maze,start,end):
             queue.append((cur[0],cur[1]-1))
     return "gg"
 
-gg = pacman_bfs(maze,start,end)
 
 def pacman_dfs(maze,start,end):
     stack = [start]
     visited = set()
     path = set()
+    height = len(maze)
+    width = len(maze[0])
     clk = time.clock()
     while stack:
         cur = stack.pop()
         if cur == end:
-            clk_used = time.clock()
+            clk_used = time.clock() - clk
             print("done")
             print("Time Used: ", clk_used, " seconds")
             return visited
@@ -61,6 +56,3 @@ def pacman_dfs(maze,start,end):
         if (cur[1]) > 0 and maze[cur[0]][cur[1]-1] == ' ' or maze[cur[0]][cur[1]-1] == '.':
             stack.append((cur[0],cur[1]-1))
     return visited
-
-wp = pacman_dfs(maze,start,end)
-
--- mainTest.py
+++ mainTest.py
@@ -2,7 +2,8 @@
 # Like the main.c in C, we put all the code here to test different method to run the maze
 
 import readTXT
-import aStar
+from aStar import *
+from DFS_BFS import *
 from utility import *
 
 "Read the maze txt file and get everything we need for searching"
@@ -10,17 +11,19 @@ from utility import *
 row, col = readTXT.get_size_txt('mediumMaze.txt')
 maze = readTXT.read_txt('mediumMaze.txt',row, col)
 start, end = readTXT.find_enter_exit(maze)
-
-path = aStar.a_star_search(maze, start, end)
+method_list = (pacman_dfs, pacman_bfs, a_star_search)
+method = 0
+path = method_list[method](maze, start, end)
 print(path)
 
-for i, j in path:
-    maze[i][j] = "."
+if method == 2:
+    for i, j in path:
+        maze[i][j] = "."
 
-withPath = open('solution.txt', 'w')
-for i in range(maze.shape[0]):
-    for j in range(maze.shape[1]):
-        withPath.write("%s" % maze[i][j])
-    withPath.write("\n")
+    withPath = open('solution.txt', 'w')
+    for i in range(maze.shape[0]):
+        for j in range(maze.shape[1]):
+            withPath.write("%s" % maze[i][j])
+        withPath.write("\n")
 
-print(path)
+    print(path)
