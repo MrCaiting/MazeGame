9acb462cdb1633b890499bf55285c61223061113
MrCaiting <cwu72@illinois.edu>
2017-09-29 21:34:19 -0500

    Minor Syntax Fixed

--- DFS_BFS.py
+++ DFS_BFS.py
@@ -4,16 +4,18 @@ from collections import deque
 
 np.set_printoptions(threshold=np.nan)
 
+
 def get_size_txt(fname):
     with open(fname) as f:
         row_count = 0
         col_count = 0
         for char in f.read():
             if char == "\n":
-                row_count= row_count + 1
+                row_count = row_count + 1
             if char != "\n":
                 col_count = col_count + 1
-    return row_count+1, int( col_count/(row_count+1))
+    return row_count+1, int(col_count/(row_count+1))
+
 
 def read_txt(fname,row,col):
     list = []
@@ -22,19 +24,22 @@ def read_txt(fname,row,col):
             ch = f.read(1)
             if ch != "\n" and ch != '':
                 list.append(ch)
-            if not ch: break
+            if not ch:
+                break
     maze = np.array(list)
-    maze = maze.reshape(row,col)
+    maze = maze.reshape(row, col)
 
     return maze
 
-row, col = get_size_txt('mediumMaze.txt')
-maze = read_txt('mediumMaze.txt',row, col)
-#print(row, col)
-#print(maze)
 
-#graph = array2graph(maze)
-#print(graph)
+row, col = get_size_txt('mediumMaze.txt')
+
+
+maze = read_txt('mediumMaze.txt', row, col)
+# print(row, col)
+# print(maze)
+# graph = array2graph(maze)
+# print(graph)
 height = len(maze)
 width = len(maze[0])
 print(height)
@@ -42,7 +47,9 @@ print(width)
 found = "found"
 
 print(maze[1][59])
-def pacman_bfs(maze,start,end):
+
+
+def pacman_bfs(maze, start, end):
     queue = deque([start])
     visited = set()
     while queue:
@@ -54,19 +61,21 @@ def pacman_bfs(maze,start,end):
         if cur in visited:
             continue
         visited.add(cur)
-        if (cur[0]+1) < height and maze[cur[0]+1][cur[1]] == ' ' or maze[cur[0]+1][cur[1]] == '.':
-            queue.append((cur[0]+1,cur[1]))
+        if (cur[0]+1) < height and maze[cur[0]+1][cur[1]] == ' ' or maze[cur[0] + 1][cur[1]] == '.':
+            queue.append((cur[0]+1, cur[1]))
         if (cur[0]) > 0 and maze[cur[0]-1][cur[1]] == ' ' or maze[cur[0]-1][cur[1]] == '.':
-            queue.append((cur[0]-1,cur[1]))
+            queue.append((cur[0]-1, cur[1]))
         if (cur[1]+1) < width and maze[cur[0]][cur[1]+1] == ' ' or maze[cur[0]][cur[1]+1] == '.':
-            queue.append((cur[0],cur[1]+1))
+            queue.append((cur[0], cur[1]+1))
         if (cur[1]) > 0 and maze[cur[0]][cur[1]-1] == ' ' or maze[cur[0]][cur[1]-1] == '.':
-            queue.append((cur[0],cur[1]-1))
+            queue.append((cur[0], cur[1]-1))
     return "gg"
 
-gg = pacman_bfs(maze,(21,1),(1,59))
 
-def pacman_dfs(maze,start,end):
+gg = pacman_bfs(maze, (21, 1), (1, 59))
+
+
+def pacman_dfs(maze, start, end):
     stack = [start]
     visited = set()
     path = set()
@@ -80,14 +89,14 @@ def pacman_dfs(maze,start,end):
             continue
         visited.add(cur)
         if (cur[0]+1) < height and maze[cur[0]+1][cur[1]] == ' ' or maze[cur[0]+1][cur[1]] == '.':
-            stack.append((cur[0]+1,cur[1]))
+            stack.append((cur[0]+1, cur[1]))
         if (cur[0]) > 0 and maze[cur[0]-1][cur[1]] == ' ' or maze[cur[0]-1][cur[1]] == '.':
-            stack.append((cur[0]-1,cur[1]))
+            stack.append((cur[0]-1, cur[1]))
         if (cur[1]+1) < width and maze[cur[0]][cur[1]+1] == ' ' or maze[cur[0]][cur[1]+1] == '.':
-            stack.append((cur[0],cur[1]+1))
+            stack.append((cur[0], cur[1]+1))
         if (cur[1]) > 0 and maze[cur[0]][cur[1]-1] == ' ' or maze[cur[0]][cur[1]-1] == '.':
-            stack.append((cur[0],cur[1]-1))
+            stack.append((cur[0], cur[1]-1))
     return visited
 
-wp = pacman_dfs(maze,(21,1),(1,59))
 
+wp = pacman_dfs(maze, (21,1), (1,59))
--- readTXT.py
+++ readTXT.py
@@ -1,37 +1,43 @@
 import numpy as np
 np.set_printoptions(threshold=np.nan)
 
+
 def get_size_txt(fname):
     with open(fname) as f:
         row_count = 0
         col_count = 0
         for char in f.read():
             if char == "\n":
-                row_count= row_count + 1
+                row_count = row_count + 1
             if char != "\n":
                 col_count = col_count + 1
-    return row_count+1, int( col_count/(row_count+1))
+    return row_count+1, int(col_count/(row_count+1))
 
-def read_txt(fname,row,col):
+
+def read_txt(fname, row, col):
     list = []
     with open(fname) as f:
         while True:
             ch = f.read(1)
             if ch != "\n" and ch != '':
                 list.append(ch)
-            if not ch: break
+            if not ch:
+                break
+
     maze = np.array(list)
-    maze = maze.reshape(row,col)
+    
+    maze = maze.reshape(row, col)
 
     return maze
+
 
 def find_enter_exit(maze):
     enter_row = 0
     enter_col = 0
     exit_row = 0
     exit_col = 0
-    for i in range(0,maze.shape[0]-1):
-        for j in range(0,maze.shape[1]-1):
+    for i in range(0, maze.shape[0]-1):
+        for j in range(0, maze.shape[1]-1):
             if maze[i][j] == "P":
                 enter_row = i
                 enter_col = j
@@ -40,9 +46,12 @@ def find_enter_exit(maze):
                 exit_col = j
     return enter_row, enter_col, exit_row, exit_col
 
+
 row, col = get_size_txt('mediumMaze.txt')
-maze = read_txt('mediumMaze.txt',row, col)
+
+
+maze = read_txt('mediumMaze.txt', row, col)
 enter_row, enter_col, exit_row, exit_col = find_enter_exit(maze)
 print(row, col)
 print(maze)
-print(maze[enter_row, enter_col],maze[exit_row,exit_col])
+print(maze[enter_row, enter_col], maze[exit_row, exit_col])
