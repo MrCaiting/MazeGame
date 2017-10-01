e6906b83627d1877524049464430bd488e470e31
unknown <Zekun>
2017-09-30 21:31:48 -0500

    heuristic

--- heuristic_multi_goal.py
+++ heuristic_multi_goal.py
@@ -1,4 +1,4 @@
-from utility import *
+import utility as util
 import numpy as np
 import MST
 
@@ -6,7 +6,7 @@ def heuristic_multi_goal(curr_child, goal_list, path_cost, closed_list, heu_val)
     dist_2goal = np.zeros(len(goal_list))
     i = 0
     for goal in goal_list:
-        dist_2goal[i] = heuristicFcn(curr_child, goal)
+        dist_2goal[i] = util.heuristicFcn(curr_child, goal)
         i += 1
     min_index = np.argmin(dist_2goal)
     min_dist_2goal = goal_list[min_index]
